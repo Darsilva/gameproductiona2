229db32747dabee37c47af0f18b767529fcb5075
AJK03 <104608990@student.swin.edu.au>
2024-04-17 00:07:04 +1000

    AU01

---
+++ My project (1)/Assets/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--- Audio Source ---")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--- Audio Clip ---")]
+    public AudioClip backmusic;
+    public AudioClip click;
+
+    private void Start()
+    {
+        musicSource.clip = backmusic;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
